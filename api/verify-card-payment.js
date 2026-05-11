2a77253068c8ff8f5810d1b31f1ced24dbaeddba
David Ellams <davidellams@hotmail.com>
2026-05-11 02:53:54 -0500

    Update verify-card-payment.js

--- api/verify-card-payment.js
+++ api/verify-card-payment.js
@@ -1,7 +1,6 @@
 
 const { createClient } = require("redis");
 const Stripe = require("stripe");
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const redis = createClient({
   url: process.env.REDIS_URL,
